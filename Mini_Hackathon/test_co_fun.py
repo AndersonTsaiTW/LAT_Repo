63a046ac7088474ae8d4bcf28f0ed13f82684775
Anderson <AndersonTsaiTW@gmail.com>
2023-05-21 14:59:02 +0800

    Update test_co_fun.py

--- Mini_Hackathon/test_co_fun.py
+++ Mini_Hackathon/test_co_fun.py
@@ -1,2 +1,8 @@
 import chatgptENG
-print(chatgptENG.chatgptfn("pineapple bun"))
+#print(chatgptENG.chatgptfn("pineapple bun"))
+
+import json
+with open("config.json") as f:
+    p = json.load(f)
+    print(p)
+
