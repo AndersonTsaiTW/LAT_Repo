def7df9850b0dbd50eab87959dd851afca9c3980
Anderson <AndersonTsaiTW@gmail.com>
2023-05-28 17:02:11 +0800

    record chatGPT's reply
    Make a new function to record the response of chatGPT, this function is to prepare for collecting user feedback later
    1. add chatGPTENG_v2
    2. add app_v3

--- Mini_Hackathon/app_v1_UrlUse.py
+++ Mini_Hackathon/app_v1_UrlUse.py
@@ -55,7 +55,6 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text.choices[0].message.content))
-#        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
--- Mini_Hackathon/test_co_fun.py
+++ Mini_Hackathon/test_co_fun.py
@@ -7,6 +7,28 @@
 #    p = json.load(f)
 #    print(p)
 
-import brtestpr1
-print(brtestpr1.breadpredict("https://i.postimg.cc/xjkRY5jt/2.jpg"))
+#import brtestpr1
+#print(brtestpr1.breadpredict("https://i.postimg.cc/xjkRY5jt/2.jpg"))
+
+import json
+#把資料寫入JSON裡面存起來
+record_file = "Bread_chat.json"
+sub_list = "baguette"
+new_chat = ["baguette is good and delicious",[0]]
+
+
+with open(record_file, "r") as file:
+    existing_data = json.load(file)
+    # 檢查麵包類別是否已存在
+    if sub_list in existing_data:
+        # 將新的描述加入現有的類別中
+        existing_data[sub_list].append(new_chat)
+    else:
+        # 若麵包類別不存在，則建立新的類別與描述
+        existing_data[sub_list] = [new_chat]
+
+# 寫入更新後的資料至JSON檔案
+with open(record_file, "w") as file:
+    json.dump(existing_data, file)
+
 
