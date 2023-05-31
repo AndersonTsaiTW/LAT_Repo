c4020b85c005efeff36a6fe486f558cc7b42e239
Shawn0604 <shawn901011@gmail.com>
2023-06-01 01:57:36 +0800

    0601
    0601

--- Mini_Hackathon(0523_ok)/config.py
+++
@@ -1,4 +0,0 @@
-channel_access_token = "k3CowvkvvyCvpDNIdK5YhgGmjjCzSQUkJtpT9ws7f4sHzCazokmxAnr5Asyx9Nh6LWYmeNnzTHATunFsY4j3EYi+Ig2n0h/USG8PYaM7zQ75iGyxLm6L9bsWcPuq1fglAQ6mwqF0XzQi8ahqnORPeAdB04t89/1O/w1cDnyilFU="
-channel_decret = "39eb7c44608310ae94a1258853b0322a"
-#ENDPOINT = "https://testdailytalk.cognitiveservices.azure.com/"
-#TEXT_ANALYTICS_API_KEY = "4eeba9991a0947dbb8c4c5bb6e9f2f83"
